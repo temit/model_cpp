eaeaa8f5db94a9dc302041b25d0a543bc7333510
temit <tboypro9@gmail.com>
2020-06-18 16:11:11 +0300

    changing the way key is handled and stored.

--- collection_type.hpp
+++ collection_type.hpp
@@ -3,47 +3,52 @@
 #include <vector>
 #include <string_view>
 #include <type_traits>
+#include <memory_resource>
 
 namespace atom
 {
-	template <typename T>
-	class collection_type
+	class collection {};
+
+	template <const char* key_value, typename type>
+	class collection_type : public collection
 	{
 	public:
-		template <typename Key, typename Value, typename = std::enable_if_t<
-												std::is_constructible_v<std::string_view, std::remove_cv_t<std::decay_t<Key>>>
-											 && std::is_constructible_v<std::vector<T>, std::remove_const_t<std::decay_t<Value>>> >>
-		constexpr collection_type(Key&& key = Key(), Value&& values = Value())
-		 : key_ { std::forward<Key>(key) },
-		   values_ { std::forward<Value>(values) }
+		template <typename Value, typename = std::enable_if_t<
+									std::is_constructible_v<std::pmr::vector<type>, std::remove_const_t<std::decay_t<Value>>>
+								>>
+		constexpr collection_type(Value&& values)
+		 : values_ { std::forward<Value>(values) }
 		{ }
-		template <typename Key, typename... Value, typename = std::enable_if_t<
-												std::is_constructible_v<std::string_view, std::remove_cv_t<std::decay_t<Key>>>
-											 && std::conjunction_v<std::is_same<T, Value>...> >>
-		constexpr collection_type(Key&& key, Value&&... values)
-		 : key_ { std::forward<Key>(key) },
-		   values_ { std::forward<Value>(values)... }
+		template <typename... Value, typename = std::enable_if_t<
+											std::conjunction_v<std::is_constructible<type, std::remove_cv_t<std::decay_t<Value>>>...>
+										>>
+		constexpr collection_type(Value&&... values)
+		 : allocator { stack_buffer, sizeof stack_buffer },
+		   values_ { { std::forward<Value>(values)... }, &allocator }
+		{ }
+		
+		constexpr collection_type()
+		 : allocator { stack_buffer, sizeof stack_buffer },
+		   values_ { &allocator }
 		{ }
 
-		[[nodiscard]] const std::string_view& key() const noexcept
+		[[nodiscard]]std::string_view key() const noexcept
 		{
 			return key_;
 		}
-		[[nodiscard]] std::string_view& key() noexcept
-		{
-			return key_;
-		}
 
-		[[nodiscard]] const std::vector<T>& values() const noexcept
+		[[nodiscard]] const std::pmr::vector<type>& values() const noexcept
 		{
 			return values_;
 		}
-		[[nodiscard]] std::vector<T>& values() noexcept
+		[[nodiscard]] std::pmr::vector<type>& values() noexcept
 		{
 			return values_;
 		}
 	private:
-		std::string_view key_;
-		std::vector<T> values_;
+		const char* key_ { key_value };
+		std::byte stack_buffer[1024];
+		std::pmr::monotonic_buffer_resource allocator;
+		std::pmr::vector<type> values_;
 	};
 }
--- test/test_collection_type.cpp
+++ test/test_collection_type.cpp
@@ -2,11 +2,25 @@
 
 #include "../collection_type.hpp"
 
+namespace keys
+{
+	const char numbers[] = "numbers";
+}
+
 TEST(model_cpp, collection_type_test_key)
 {
-	atom::collection_type<int> grades("grades");
-	EXPECT_EQ("grades", grades.key());
+	atom::collection_type<keys::numbers, int> grades;
+	EXPECT_EQ(keys::numbers, grades.key());
+}
 
-	atom::collection_type<int> other_grades("grades", 90, 100, 99, 98, 94);
-	EXPECT_EQ("grades", other_grades.key());
+TEST(model_cpp, collection_type_test_key_with_vardiac_argument)
+{
+	atom::collection_type<keys::numbers, int> grades(1, 2, 3, 4, 5);
+	EXPECT_EQ(keys::numbers, grades.key());
+}
+
+TEST(model_cpp, collection_type_test_key_with_vector)
+{
+	atom::collection_type<keys::numbers, int> grades(std::pmr::vector<int>{ 1, 2, 3, 4, 5 });
+	EXPECT_EQ(keys::numbers, grades.key());
 }
